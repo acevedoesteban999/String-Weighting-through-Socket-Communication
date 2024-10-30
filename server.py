1c0a4be51080fb40229fb065371caa8c9334a063
Esteban <acevedoesteban999@gmail.com>
2024-10-30 15:30:58 -0400

    MAIN FILES: Show Time for client  , configure loggin

--- server.py
+++ server.py
@@ -6,9 +6,15 @@ import sys
 from pathlib import Path
 sys.path[0] = str(Path(sys.path[0]).parent)
 
+# Loggin configuration
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)s]: %(message)s',  
+    datefmt='%Y-%m-%d %H:%M:%S'                         
+)
 if __name__ == "__main__":
     
-    server_socket = serverSocketHanlder()
-    server_socket.socket_handler_init()
+    server_socket = serverSocketHanlder()   # Init hanlder
+    
+    server_socket.socket_handler_init()     # Main lopp for server
         
     
