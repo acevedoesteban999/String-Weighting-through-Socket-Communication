877e93e9f3f0b83272d42a5a7ef1953280f0b7ee
Esteban <acevedoesteban999@gmail.com>
2024-10-30 16:13:50 -0400

    COMMENTS

--- src/client_socket_handler.py
+++ src/client_socket_handler.py
@@ -1,6 +1,8 @@
 from .socket.socket_handler import socketHandler
 import logging
 
+MAX_ITEMS_TO_SEND = 10
+
 class clientSocketHanlder(socketHandler):   
     def socket_handler_init(self) -> bool:
         """
@@ -16,9 +18,10 @@ class clientSocketHanlder(socketHandler):
         return False
     
     
-    def send_strings_data(self,data_str:str,max = 1) -> str:
+    def send_strings_data(self,data_str:str) -> str:
         """
             Send strings agrupadted to server
+                - This method use MAX_ITEMS_TO_SEND global variable
         """
         
         # To group, all values will be stored in a string, and with a counter, the send event will be triggered
@@ -27,21 +30,22 @@ class clientSocketHanlder(socketHandler):
         count_str_grouped = 0
         for line in data_str:
             buffer_str += line                                  # Group strings 
-            count_str_grouped += 1                              # Inc count  
-            if count_str_grouped >= max:   
+            count_str_grouped += 1                              # Increment counter  
+            if count_str_grouped >= MAX_ITEMS_TO_SEND:   
                 self._socket.sendall(buffer_str.encode())       # Its necesary convert to binay ( str.encode, by default utf-8)
                 response += self._socket.recv(1024).decode()    # Its necesary convert to str ( binary.decode, by default utf-8)
                 
-                #Reset buffer and count
+                #Reset buffer and counter
                 buffer_str = ""
                 count_str_grouped = 0
                 
         # Before finishing the loop, it's necessary to check if there is any data that has not been sent yet
         if count_str_grouped:
-            self._socket.sendall((buffer_str + "\n\r").encode())      
+            self._socket.sendall((buffer_str + "\n\r").encode())   # Send data and the end signal    
             response += self._socket.recv(1024).decode()   
         else:
-            self._socket.sendall("\n\r".encode())  
+            self._socket.sendall("\n\r".encode())                   # Send only the end signal
+            
         return response
          
         
